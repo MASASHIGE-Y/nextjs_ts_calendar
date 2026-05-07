50cea15d9790fee883ca000560755920b3b23a62
MASASHIGE-Y <da12ran2berl8.remuni38@gmail.com>
2026-05-07 20:46:05 +0900

    fix: migrate sqlite to postgres with neon

--- prisma/migrations/20260426004452_init/migration.sql
+++
@@ -1,6 +0,0 @@
--- CreateTable
-CREATE TABLE "Event" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "date" TEXT NOT NULL,
-    "title" TEXT NOT NULL
-);
---
+++ prisma/migrations/20260507113635_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" TEXT NOT NULL,
+    "date" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
